6129aa4a8db4a8162334653cfab8e41957bce504
Fantix King <fantix.king@gmail.com>
2019-12-30 14:43:05 -0600

    add GA and fix Makefile

--- src/src/main.js
+++ src/src/main.js
@@ -3,8 +3,14 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import VueGtag from 'vue-gtag'
 
 Vue.config.productionTip = false
+if (process.env['VUE_APP_GAID']) {
+  Vue.use(VueGtag, {
+    config: { id: process.env['VUE_APP_GAID'] }
+  }, router)
+}
 
 new Vue({
   router,
